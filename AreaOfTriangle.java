00518ce6e01bb8948e101386bb23edd88a1fff38
Aniket Panpatil <88042871+Aniket121397@users.noreply.github.com>
2021-12-19 21:05:49 +0530

    area triangle

---
+++ AreaOfTriangle.java
@@ -0,0 +1,9 @@
+public class AreaOfTriangle {
+                    public static void main(String[] args) {
+                           int b = 5 , h = 6;
+                           float Area = 0.5f * b * h;
+                           System.out.println(Area);
+
+                        }
+    
+}
