a84fd4a6b0514f6a25e51c96397079e544b9e303
Aniket Panpatil <88042871+Aniket121397@users.noreply.github.com>
2021-12-19 21:06:09 +0530

    area circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        float pi = 3.14f ;
+        int r = 5;
+        float Area = pi * r * r;
+        System.out.println(Area);
+    }
+    
+}
