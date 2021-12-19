3898fac370c22daa57343f83f0fc6cd50a8aa1a8
Aniket Panpatil <88042871+Aniket121397@users.noreply.github.com>
2021-12-19 21:05:01 +0530

    print first program

---
+++ Print.java
@@ -0,0 +1,9 @@
+class FirstProgram
+    {
+            public static void main(String args[]) {
+
+                System.out.println(10);
+                System.out.println(20);
+                
+            }
+    }
