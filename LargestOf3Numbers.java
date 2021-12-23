cb0440a5f49823fe6f56f18f06642924b7d87aba
Aniket Panpatil <88042871+Aniket121397@users.noreply.github.com>
2021-12-23 19:52:28 +0530

    Add files via upload

---
+++ LargestOf3Numbers.java
@@ -0,0 +1,7 @@
+class LargestOf3Numbers {
+                        public static void main(String[] args) {
+                            int a = 10 , b = 20 , c = 5 ;
+                            System.out.println(a>b ? (a > c ? a:c):(b>c?b:c));
+                            
+                        } 
+}
